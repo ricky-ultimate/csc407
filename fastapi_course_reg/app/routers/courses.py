afd7bddde6e2c62bab60d67021f4ff3aa25f4af8
Ricky Ultimate <rickyogigi@gmail.com>
2025-11-09 14:12:22 +0100

    chore: supress warnings

--- fastapi_course_reg/app/routers/courses.py
+++ fastapi_course_reg/app/routers/courses.py
@@ -6,7 +6,7 @@ router = APIRouter(prefix="/courses", tags=["Courses"])
 
 @router.post("/", response_model=CourseOut)
 async def create_course(course: CourseCreate):
-    return await db.course.create(data=course.dict())
+    return await db.course.create(data=course.dict()) # type: ignore
 
 @router.get("/", response_model=list[CourseOut])
 async def list_courses():
